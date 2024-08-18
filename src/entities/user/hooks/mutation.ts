08d8daca7e8debcf102c769f6ea983988d7b5b26
Gn0lee <jinho605@gmail.com>
2024-08-18 22:47:14 +0900

    feat: 로그인 세션 처리

--- src/entities/user/hooks/mutation.ts
+++ src/entities/user/hooks/mutation.ts
@@ -2,6 +2,9 @@ import { useMutation } from '@tanstack/react-query';
 import supabaseInstance from 'src/shared/ky/supabaseInstance';
 import { useNavigate } from '@tanstack/react-router';
 import { useToast } from '@chakra-ui/react';
+import { User } from 'src/entities/user/types/user';
+import { Auth } from 'src/entities/user/types/auth';
+import { SESSION_STORAGE_KEYS } from 'src/shared/constants/storage';
 
 const FAIL_TOAST_ID = 'fail-toast';
 
@@ -12,15 +15,18 @@ export const usePostOauthKakaoCallback = () => {
 
 	return useMutation({
 		mutationFn: (code: string) =>
-			supabaseInstance.post('oauth/kakao', {
-				body: JSON.stringify({ code, redirect_uri: 'http://localhost:5173/oauth/kakao/callback' }),
-			}),
+			supabaseInstance
+				.post('oauth/kakao', {
+					body: JSON.stringify({ code, redirect_uri: import.meta.env.VITE_KAKAO_CALLBACK_URL }),
+				})
+				.json<{ user: User } & Auth>(),
 		onSuccess: data => {
-			console.log(data);
+			sessionStorage.setItem(SESSION_STORAGE_KEYS.ACCESS_TOKEN, data.access_token);
+			sessionStorage.setItem(SESSION_STORAGE_KEYS.REFRESH_TOKEN, data.refresh_token);
 
 			navigate({ to: '/' });
 		},
-		onError: error => {
+		onError: () => {
 			if (!toast.isActive(FAIL_TOAST_ID)) {
 				toast({
 					title: '로그인에 실패했습니다.\n다시 시도해주세요.',
@@ -32,9 +38,7 @@ export const usePostOauthKakaoCallback = () => {
 				});
 			}
 
-			console.log(error);
-
-			// navigate({ to: '/user' });
+			navigate({ to: '/user' });
 		},
 		retry: false,
 	});
