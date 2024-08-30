b95c74eff10df117e1c4c551477b5976d3aaf3a3
Gn0lee <jinho605@gmail.com>
2024-08-31 03:04:00 +0900

    fix: 쿠키 헤더 수정

--- src/entities/user/lib/auth.ts
+++ src/entities/user/lib/auth.ts
@@ -11,19 +11,18 @@ const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env
 			},
 			setItem: (key, value) => {
 				Cookies.set(key, value, {
-					expires: 365,
-					secure: import.meta.env.PROD,
-					domain: import.meta.env.VITE_COOKIE_DOMAIN,
 					path: '/',
-					httpOnly: true,
+					httpOnly: false,
+					sameSite: 'none',
+					secure: true,
 				});
 			},
 			removeItem: key => {
 				Cookies.remove(key, {
-					secure: import.meta.env.PROD,
-					domain: import.meta.env.VITE_COOKIE_DOMAIN,
 					path: '/',
-					httpOnly: true,
+					httpOnly: false,
+					sameSite: 'none',
+					secure: true,
 				});
 			},
 		},
